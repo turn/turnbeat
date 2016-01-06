62df7a4383bf89f69af5c5d6be1523b57edb4914
Jonathan Creasy <jonathan.creasy@gmail.com>
2016-01-06 10:53:44 -0800

    implement jsonexpander filter

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/blacklightops/libbeat/filters"
 	"github.com/blacklightops/libbeat/filters/nop"
 	"github.com/blacklightops/libbeat/filters/opentsdb"
+  "github.com/blacklightops/libbeat/filters/jsonexpander"
 	"github.com/blacklightops/libbeat/logp"
 	"github.com/blacklightops/libbeat/publisher"
 	"github.com/blacklightops/turnbeat/config"
@@ -25,6 +26,7 @@ var Name = "turnbeat"
 var EnabledFilterPlugins map[filters.Filter]filters.FilterPlugin = map[filters.Filter]filters.FilterPlugin{
 	filters.NopFilter:      new(nop.Nop),
 	filters.OpenTSDBFilter: new(opentsdb.OpenTSDB),
+  filters.JSONExpanderFilter: new(jsonexpander.JSONExpander),
 }
 
 func main() {
